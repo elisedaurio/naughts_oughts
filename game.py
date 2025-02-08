237672d1f21020386b1a800b80aa18e55739be08
Elise Daurio <5331011+elisedaurio@users.noreply.github.com>
2025-02-08 16:29:21 -0700

    Add a Turn class. Add game_over to the model for a game

--- game.py
+++ game.py
@@ -8,16 +8,29 @@ class Row(BaseModel):
     col_2: Optional[str] = ""
     col_3: Optional[str] = ""
 
+# A turn consists of an iterator, the player who takes the turn, and their selection.
+#
+# If a user attempts an invalid action, that doesn't "count" for their turn being executed.
+class Turn(BaseModel):
+    # Starts at 1, iterates until win.
+    turn_number: int
+    player: str
+    row: int
+    col: int    
+
 class Game(BaseModel):
     id: Optional[PydanticObjectId] = None
     player_id: uuid.UUID
     game_board: List[Row]
     current_turn: int
+    turn_history: List[Turn]
+    game_over: bool
 
 class GameStorage(AbstractRepository[Game]):
     class Meta:
         collection_name = "games"
 
-def check_db_for_player(player_id):
-    # Check the DB to see if the player ID exists.
+# This will be defined if I have time.
+#def check_db_for_player(player_id):
+#    # Check the DB to see if the player ID exists.
     
