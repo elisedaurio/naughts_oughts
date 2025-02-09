1138c9abe3a4b59c49c801cb681d3d622d46ad12
Elise Daurio <5331011+elisedaurio@users.noreply.github.com>
2025-02-09 15:55:25 -0700

    Make several updates to the model and partially complete validation.

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from pydantic_mongo import PydanticObjectId, AbstractRepository
 from typing import List, Optional
+from pymongo import MongoClient
 import uuid
 import logging
 
@@ -17,17 +18,26 @@ class Row(BaseModel):
 class Turn(BaseModel):
     # Starts at 1, iterates until win.
     # Must be provided when posting a move in order to make sure you aren't cheating~
+    game_id: uuid
     turn_number: int
-    player: str
+    player: uuid
     row: int
     col: int    
 
 class Game(BaseModel):
+    # The ID of the game
     id: Optional[PydanticObjectId] = None
+    # UUID of the human player
     player_id: uuid.UUID
+    # Active player is either the CPU or the player. Capture the string representation here
+    active_player: str = None
+    # The current state of the game board. A list of three rows
     game_board: List[Row]
+    # The current turn number. (Tracking how many turns have occurred.)
     current_turn: int
+    # Capture each turn as it happens
     turn_history: List[Turn] = None
+    # Track if the game has ended
     game_over: bool = False
 
 class GameStorage(AbstractRepository[Game]):
@@ -36,8 +46,8 @@ class GameStorage(AbstractRepository[Game]):
 
 # Submit a turn
 # This function is the abstraction of a turn into something uniform to save sanity for devs who dare to look at the main.py file.
-def submit_turn(submitted_turn):
-    turn_validation = validate_turn(submitted_turn)
+def submit_turn(submitted_turn, db_storage):
+    turn_validation = validate_turn(submitted_turn, db_storage)
     if turn_validation == True:
         executed_turn = execute_turn(submitted_turn)
         if executed_turn == True:
@@ -54,9 +64,35 @@ def submit_turn(submitted_turn):
     
 # Make sure the turn is valid.
 # This will return true if the turn is valid from what we can tell.
-def validate_turn(incoming_turn, game_id):
+def validate_turn(incoming_turn: Turn, db_storage:GameStorage):
     # This is a set of the rules needed to be followed to make sure a turn is valid.
+    
+    # Load the game from the game_id
+    current_game = Game(db_storage.find_one_by_id(incoming_turn.game_id))
+    
+    
+    
+    # Validation list:
+    # Is the game over?
+    if current_game.game_over == True:
+        # Failed
+        logging.info("Game with ID: " + f"{current_game.id}" + " is already over.")
+        return False
+    
+    # Was the right turn submitted?
+    if current_game.current_turn != incoming_turn.turn_number:
+        # The wrong turn was submitted
+        logging.info("Game with ID: " + f"{current_game.id}" + " is not currently on the submitted turn of: " + f"{incoming_turn.turn_number}" + ". It is on turn: " + f"{current_game.current_turn}")
+        return False
+    # Is this right player taking a turn?
+    if current_game.
+    # Was x and y 1,2 or 3?
+    # Was the the (x,y) coordinate Null or populated?
+    
+    is_game_over = 
     print("Validated the turn. Proceed.")
+    return(True)
+    
 
 # Attempt to execute a turn
 # By the end of this function, we should have commited the turn the DB or returned an error. 
--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import bson
 import logging
 from uuid import uuid4
 from pymongo import MongoClient
-from pymongo import collection
-from pymongo import InsertOne
 from random import randint
 from fastapi import FastAPI
 from game import Row
@@ -97,8 +95,13 @@ def start_new_game(player_id = None):
 # Allows me to make the next move by specifying the co-ordinates I wish to move on. e.g. {"x": 1, "y": 1} 
 # would denote a move to the middle square by the requesting player, and returns the new state of the board after the computer has made its move in turn. 
 # Note: There is no need to create an AI opponent, random moves are fine
-@app.post("/no/play/{game_id}")
-def play_game_turn(turn: Turn):
+@app.post("/no/playturn")
+def play_game_turn(incoming_turn: Turn):
+    # Submit a turn
+    
+    # This will require the game_id, the turn contents, and a reference to the storage collection.
+    submit_turn(incoming_turn, game_id, db_storage)
+    
     return {"message":"Play a turn on a game"}
 
 # Return the plays done in order of a given game
